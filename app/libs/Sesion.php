4f0a8142ff92932c121a1b7d7b83ea4cf3f65b42
Taoli Garaudy Solano Mendoza <garaudy12@gmail.com>
2021-05-25 23:20:00 -0500

    Login cafe
    login cafe huerto

---
+++ app/libs/Sesion.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Manejar sesión
+ */
+class Sesion{
+  private $login = false;
+  private $usuario;
+  
+  function __construct()
+  {
+    session_start();
+    if (isset($_SESSION["usuario"])) {
+      $this->usuario = $_SESSION["usuario"];
+      $this->login = true;
+    } else {
+      unset($this->usuario);
+      $this->login = false;
+    }
+  }
+
+  public function iniciarLogin($usuario){
+    if ($usuario) {
+      $this->usuario = $_SESSION["usuario"] = $usuario;
+      $this->login = true;
+    }
+  }
+
+  public function finalizarLogin(){
+    unset($_SESSION["usuario"]);
+    unset($this->usuario);
+    session_destroy();
+    $this->login = false;
+  }
+
+  public function getLogin(){
+    return $this->login;
+  }
+
+  public function getUsuario(){
+    return $this->usuario;
+  }
+}
+
+?>
